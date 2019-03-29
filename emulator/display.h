f8404015d0cb098f9b94a2e7ecbc267791c0ff1d
Juhana Jauhiainen <juhana.jauhiainen@gmail.com>
2019-03-29 21:48:59 +0200

    Added some SDL initialization code to Display.

--- emulator/display.h
+++ emulator/display.h
@@ -1,16 +1,42 @@
 #ifndef DISPLAY_H
 #define DISPLAY_H
 
+#include <SDL.h>
+#include <SDL_opengl.h>
 
 /* Display handles drawing the Chip8 screen contents.
  * The actual screen data is stored in emulator memory
  * and it's passed to the update method.
  */
 class Display {
- public:
-  void update(const uint8_t &screen);
-  void clear(uint8_t *screen);
+public:
+  Display() {}
+  ~Display() {
+    SDL_GL_DeleteContext(m_glcontext);
+    SDL_DestroyWindow(m_window);
+    SDL_Quit();
+  }
 
-}
+  /* Initializes SDL and creates a window and OpenGL context for rendering */
+  int init() {
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+      SDL_Log("Failed to initialize SDL: %s", SDL_GetError());
+      return 1;
+    }
+
+    m_window =
+        SDL_CreateWindow("Chip8", SDL_WINDOWPOS_UNDEFINED,
+                         SDL_WINDOWPOS_UNDEFINED, 64, 32, SDL_WINDOW_OPENGL);
+
+    m_glcontext = SDL_GL_CreateContext(m_window);
+    return 0;
+  }
+
+  void update(const uint8_t &screen);
+
+private:
+  SDL_Window *m_window = nullptr;
+  SDL_GLContext m_glcontext;
+};
 
 #endif
--- emulator/emulator.cpp
+++ emulator/emulator.cpp
@@ -4,12 +4,14 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-class chip8 {
+#include "display.h"
+
+class Chip8 {
 public:
-  chip8() {
-    this->memory = new unsigned char[1024 * 4 + 0x200]; // 4K memory reserved
-    this->SP = 0xfa0;
-    this->PC = 0x200; // Programs are loaded at 0x200
+  Chip8() {
+    m_memory = new unsigned char[1024 * 4 + 0x200]; // 4K memory reserved
+    m_SP = 0xfa0;
+    m_PC = 0x200; // Programs are loaded at 0x200
   }
 
   void load_rom(char *filename) {
@@ -24,13 +26,13 @@ public:
 
     // Load rom to memory at 0x200
     rom.seekg(0, std::ios::beg);
-    rom.read((char *)this->memory + 0x200, size);
+    rom.read((char *)m_memory + 0x200, size);
     rom.close();
   }
 
   void emulate() {
     // Fetch first 8 bits of next instruction
-    uint8_t *op = &this->memory[this->PC];
+    uint8_t *op = &m_memory[m_PC];
 
     int highnib = (*op & 0xf0) >> 4;
     switch (highnib) {
@@ -40,51 +42,51 @@ public:
       // JUMP $NNN
       // Jump to NNN
       uint16_t target = ((op[0] & 0xf) << 8) | op[1];
-      this->PC = target;
+      m_PC = target;
     } break;
     case 0x02: {
       // CALL $NNN
       // Call subroutine at NNN
 
       // Advance stack pointer
-      this->SP = -2;
+      m_SP = -2;
 
       // Store next instructions address to memory pointed by the stack pointer
-      memory[this->SP] = ((this->PC + 2) & 0xff00) >> 8;
-      memory[this->SP + 1] = ((this->PC + 2) & 0x00ff);
+      m_memory[m_SP] = ((m_PC + 2) & 0xff00) >> 8;
+      m_memory[m_SP + 1] = ((m_PC + 2) & 0x00ff);
 
       // Jump to subroutines address NNN
       uint16_t target = ((op[0] & 0xf) << 8) | op[1];
-      this->PC = target;
+      m_PC = target;
     } break;
     case 0x03: {
       uint8_t reg = op[0] & 0x0f;
-      if (this->V[reg] == op[1])
-        this->PC += 2;
-      this->PC += 2;
+      if (m_V[reg] == op[1])
+        m_PC += 2;
+      m_PC += 2;
     } break;
     case 0x04: {
       uint8_t reg = op[0] & 0x0f;
-      if (this->V[reg] != op[1])
-        this->PC += 2;
-      this->PC += 2;
+      if (m_V[reg] != op[1])
+        m_PC += 2;
+      m_PC += 2;
     } break;
     case 0x05: {
       uint8_t reg1 = op[0] & 0x0f;
       uint8_t reg2 = (op[1] & 0xf0) >> 4;
-      if (this->V[reg1] == this->V[reg2])
-        this->PC += 2;
-      this->PC += 2;
+      if (m_V[reg1] == m_V[reg2])
+        m_PC += 2;
+      m_PC += 2;
     } break;
     case 0x06: {
       uint8_t reg = op[0] & 0x0f;
-      this->V[reg] = op[1];
-      this->PC += 2;
+      m_V[reg] = op[1];
+      m_PC += 2;
     } break;
     case 0x07: {
       uint8_t reg = op[0] & 0x0f;
-      this->V[reg] += op[1];
-      this->PC += 2;
+      m_V[reg] += op[1];
+      m_PC += 2;
     } break;
     case 0x08: {
       uint8_t command = op[1] & 0x0f;
@@ -93,59 +95,59 @@ public:
 
       switch (command) {
       case 0x1: {
-        this->V[reg1] |= this->V[reg2];
+        m_V[reg1] |= m_V[reg2];
       } break;
       case 0x2: {
-        this->V[reg1] &= this->V[reg2];
+        m_V[reg1] &= m_V[reg2];
       } break;
       case 0x3: {
-        this->V[reg1] ^= this->V[reg2];
+        m_V[reg1] ^= m_V[reg2];
       } break;
       case 0x4: {
-        uint16_t result = this->V[reg1] + this->V[reg2];
-        this->V[0xf] = (result & 0xff00) ? 1 : 0;
-        this->V[reg1] = result & 0xff;
+        uint16_t result = m_V[reg1] + m_V[reg2];
+        m_V[0xf] = (result & 0xff00) ? 1 : 0;
+        m_V[reg1] = result & 0xff;
       } break;
       case 0x5: {
-        uint8_t result = this->V[reg1] - this->V[reg2];
-        this->V[0xf] = (result > 0) ? 1 : 0;
-        this->V[reg1] = result;
+        uint8_t result = m_V[reg1] - m_V[reg2];
+        m_V[0xf] = (result > 0) ? 1 : 0;
+        m_V[reg1] = result;
       } break;
       case 0x6: {
-        this->V[0xf] = this->V[reg1] & 0x1;
-        this->V[reg1] = this->V[reg1] >> 1;
+        m_V[0xf] = m_V[reg1] & 0x1;
+        m_V[reg1] = m_V[reg1] >> 1;
       } break;
       case 0x7: {
-        uint8_t result = this->V[reg2] - this->V[reg1];
-        this->V[0xf] = (result > 0) ? 1 : 0;
-        this->V[reg1] = result;
+        uint8_t result = m_V[reg2] - m_V[reg1];
+        m_V[0xf] = (result > 0) ? 1 : 0;
+        m_V[reg1] = result;
       } break;
       case 0xe: {
-        this->V[0xf] = (0x80 == (this->V[reg1] & 0x80));
-        this->V[reg1] = this->V[reg1] << 1;
+        m_V[0xf] = (0x80 == (m_V[reg1] & 0x80));
+        m_V[reg1] = m_V[reg1] << 1;
       } break;
       }
-      this->PC += 2;
+      m_PC += 2;
     } break;
     case 0x09: {
       uint8_t reg1 = op[0] & 0xf;
       uint8_t reg2 = (op[1] & 0xf0) >> 4;
-      if (this->V[reg1] != this->V[reg2])
-        this->PC += 2;
-      this->PC += 2;
+      if (m_V[reg1] != m_V[reg2])
+        m_PC += 2;
+      m_PC += 2;
     }
     case 0x0a: {
-      this->I = ((op[0] & 0xf) << 8) | op[1];
-      this->PC += 2;
+      m_I = ((op[0] & 0xf) << 8) | op[1];
+      m_PC += 2;
     } break;
     case 0x0b: {
       uint16_t num = ((op[0] & 0xf) << 8) | op[1];
-      this->PC = this->V[0] + num;
+      m_PC = m_V[0] + num;
     } break;
     case 0x0c: {
       uint8_t reg = op[0] & 0xf;
       uint8_t num = op[1];
-      this->V[reg] = (rand() % 256) + num;
+      m_V[reg] = (rand() % 256) + num;
     } break;
     case 0x0d: {
       // TODO: Drawing sprites
@@ -157,19 +159,19 @@ public:
       int reg = op[0] & 0xf;
       switch (op[1]) {
       case 0x07: {
-        this->V[reg] = this->delay;
+        m_V[reg] = m_delay;
       } break;
       case 0x0A:
         // Not implemented yet
         break;
       case 0x15: {
-        this->delay = this->V[reg];
+        m_delay = m_V[reg];
       } break;
       case 0x18: {
-        this->sound = this->V[reg];
+        m_sound = m_V[reg];
       } break;
       case 0x1e: {
-        this->I = this->V[reg] + this->I;
+        m_I = m_V[reg] + m_I;
       } break;
       case 0x29:
         // Not implemented yet
@@ -180,49 +182,49 @@ public:
          * locations I, I+1 and I+2.
          */
         uint8_t ones, tens, hundreds;
-        uint8_t value = this->V[reg];
+        uint8_t value = m_V[reg];
         ones = value % 10;
         value /= 10;
         tens = value % 10;
         hundreds = value / 10;
-        this->memory[this->I] = hundreds;
-        this->memory[this->I + 1] = tens;
-        this->memory[this->I + 2] = ones;
+        m_memory[m_I] = hundreds;
+        m_memory[m_I + 1] = tens;
+        m_memory[m_I + 2] = ones;
       } break;
       case 0x55: {
         /* Fx55 - LD [I], Vx
          * Store registers V0 to Vx in memory starting at location I.
          */
         for (auto i = 0; i < reg; i++)
-          this->memory[this->I + i] = this->V[reg + i];
+          m_memory[m_I + i] = m_V[reg + i];
       } break;
       case 0x65: {
         /* Fx65 - LD Vx, [I]
          * Read registers V0 to Vx from memory starting at location I.
          */
         for (auto i = 0; i < reg; i++)
-          this->V[reg + i] = this->memory[this->I + i];
+          m_V[reg + i] = m_memory[m_I + i];
       } break;
       }
-      this->PC += 2;
+      m_PC += 2;
     } break;
     }
   }
 
 private:
-  uint8_t V[16]; // Registers 0-F
-  uint16_t I;    // Index register
-  uint16_t SP;   // Stack pointer
-  uint16_t PC;   // Program counter
-  uint8_t delay; // Delay timer
-  uint8_t sound; // Sound timer
-  uint8_t *memory;
-  uint8_t *screen; // Same as memory[0xF00]
+  uint8_t m_V[16]; // Registers 0-F
+  uint16_t m_I;    // Index register
+  uint16_t m_SP;   // Stack pointer
+  uint16_t m_PC;   // Program counter
+  uint8_t m_delay; // Delay timer
+  uint8_t m_sound; // Sound timer
+  uint8_t *m_memory;
+  uint8_t *m_screen; // Same as memory[0xF00]
 };
 
 int main(int argc, char **argv) {
-  chip8 vm;
-
+  Chip8 vm;
   vm.load_rom(argv[1]);
+
   return 0;
 }

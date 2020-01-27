d1383fd5ed852cf535ced9e59682ad3440163d58
Juhana Jauhiainen <juhana.jauhiainen@gmail.com>
2020-01-27 21:58:01 +0200

    Added tests for SUB

---
+++ tests/sub.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+
+import re
+import os
+import json
+
+from util import run_asm
+
+def test_sub_register():
+    asm = """
+        LD V0, #1
+        LD V1, #1
+        SUB V0, V1
+        EXIT
+    """
+    emulator_debug = run_asm(asm)
+    assert emulator_debug.get("V0") == 0
+
+def test_sub_register_with_carry():
+    asm = """
+        LD V0, #f
+        LD V1, #a
+        SUB V0, V1
+        EXIT
+    """
+    emulator_debug = run_asm(asm)
+    print(emulator_debug)
+    assert emulator_debug.get("V0") == 1
+    assert emulator_debug.get("VF") == 1

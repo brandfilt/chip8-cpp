d0ed280a7c10baf977534fa95ddcf23c1860acc3
Juhana Jauhiainen <juhana.jauhiainen@gmail.com>
2020-02-03 22:12:24 +0200

    Fixed SUBN tests.

--- tests/subn.py
+++ tests/subn.py
@@ -6,12 +6,24 @@ import json
 
 from util import run_asm
 
-def test_subn_register():
+def test_subn_not_borrow():
     asm = """
         LD V0, #1
-        LD V1, #1
-        SUB V0, V1
+        LD V1, #2
+        SUBN V0, V1
         EXIT
     """
     emulator_debug = run_asm(asm)
-    assert emulator_debug.get("V0") == 0
+    assert emulator_debug.get("V0") == 1
+    assert emulator_debug.get("VF") == 1
+
+def test_subn_borrow():
+    asm = """
+        LD V0, #2
+        LD V1, #1
+        SUBN V0, V1
+        EXIT
+    """
+    emulator_debug = run_asm(asm)
+    assert emulator_debug.get("V0") == 255
+    assert emulator_debug.get("VF") == 0
